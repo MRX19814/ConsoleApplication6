547fc360ed21fbf0ff5cde4b6b4f6e1d234cf777
MRX <kurganovss666@gmail.com>
2023-05-20 23:11:39 +0300

    Базовый уровень

--- ConsoleApplication6/Program.cs
+++ ConsoleApplication6/Program.cs
@@ -1,36 +1,84 @@
-﻿abstract class Delivery
+﻿using System;
+using System.Collections.Generic;
+
+abstract class Delivery // обстрактный класс
 {
-    public string Address;
+    public string Address { get; set; }
 }
 
-class HomeDelivery : Delivery
+class HomeDelivery : Delivery //  наследуется от базового класса Delivery
 {
-    /* ... */
+    public string CourierName { get; set; }
 }
 
-class PickPointDelivery : Delivery
+class PickPointDelivery : Delivery // наследуется от базового класса Delivery
 {
-    /* ... */
+    public string CompanyName { get; set; }
+    public string PickupPoint { get; set; }
 }
 
-class ShopDelivery : Delivery
+class ShopDelivery : Delivery // наследуется от базового класса Delivery
 {
-    /* ... */
+    public string ShopName { get; set; }
 }
 
-class Order<TDelivery,
-TStruct> where TDelivery : Delivery
+class Product
 {
-    public TDelivery Delivery;
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+}
 
-    public int Number;
+class Order <TDelivery> where TDelivery : Delivery // используются конструкторы с параметрами, класс использует обобщение TDelivery, содержит объект класса TDelivery в свойстве Delivery, а также список объектов класса Product в свойстве Products
+{
+    public TDelivery Delivery { get; set; }
+    public int Number { get; set; }
+    public string Description { get; set; }
+    public List<Product> Products { get; set; }
 
-    public string Description;
-
-    public void DisplayAddress()
+    public Order(TDelivery delivery, int number, string description)
     {
-        Console.WriteLine(Delivery.Address);
+        Delivery = delivery;
+        Number = number;
+        Description = description;
+        Products = new List<Product>();
     }
 
-    // ... Другие поля
+    public void AddProduct(Product product)
+    {
+        Products.Add(product);
+    }
+
+    public void RemoveProduct(Product product)
+    {
+        Products.Remove(product);
+    }
+
+    public decimal CalculateTotalPrice()
+    {
+        decimal totalPrice = 0;
+        foreach (var product in Products)
+        {
+            totalPrice += product.Price;
+        }
+        return totalPrice;
+    }
+}
+
+class Address // в классе переопределен метод ToString, используются конструкторы с параметрами
+{
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string PostalCode { get; set; }
+
+    public Address(string street, string city, string postalCode)
+    {
+        Street = street;
+        City = city;
+        PostalCode = postalCode;
+    }
+
+    public override string ToString()
+    {
+        return $"{Street}, {City}, {PostalCode}";
+    }
 }
